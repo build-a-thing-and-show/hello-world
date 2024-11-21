f72fb59b409af1497babd8467b2c902ed27668ed
Amin <aminghz@gmail.com>
2024-11-20 22:55:05 -0800

    change the port

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 // Define a route
 app.get('/', (req: Request, res: Response) => {
